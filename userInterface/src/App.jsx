9ef36bc4d25a61df4e00553a3626b9b3d821899d
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-10-11 15:34:13 +0300

    Initialized creation of objects for the assets

--- userInterface/src/App.jsx
+++ userInterface/src/App.jsx
@@ -1,34 +1,36 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Profile from './Pages/Profile'
-import Contact from './Pages/Contact'
-import Login from './Pages/Login'
-import ShoppingCart from './Pages/ShoppingCart'
-import AboutZulu from './Pages/AboutZulu'
-import Products from './Pages/Products'
-import Placeorder from './Pages/Placeorder'
-import HistoryOrder from './Pages/HistoryOrder'
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Profile from './Pages/Profile';
+import Contact from './Pages/Contact';
+import Login from './Pages/Login';
+import ShoppingCart from './Pages/ShoppingCart';
+import AboutZulu from './Pages/AboutZulu';
+import Products from './Pages/Products';
+import Placeorder from './Pages/Placeorder';
+import HistoryOrder from './Pages/HistoryOrder';
+import NotFound from './Pages/NotFound';
+import Navigationbar from './Components/Navigationbar'
 
 const App = () => {
   return (
-    <div className='Screen-Compatible'>
+    <div className='screen-compatible'>
+      <Navigationbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/Profile' element={<Profile />} />
-        <Route path='/Contact' element={<Contact />} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Cart' element={<ShoppingCart />} />
-        <Route path='/About' element={<AboutZulu />} />
-        <Route path='/Drinks' element={<Products />} />
-        <Route path='/Order' element={<Placeorder />} />
-        <Route path='/History' element={<HistoryOrder />} />
-
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/cart' element={<ShoppingCart />} />
+        <Route path='/about' element={<AboutZulu />} />
+        <Route path='/drinks' element={<Products />} />
+        <Route path='/order' element={<Placeorder />} />
+        <Route path='/history' element={<HistoryOrder />} />
+        {/* Optional: Fallback Route */}
+        <Route path='*' element={<NotFound />} /> *
       </Routes>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ userInterface/src/Components/Navigationbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navigationbar = () => {
+    return (
+        <div className='navbar'>
+            <img src='' />
+
+        </div>
+    )
+}
+
+export default Navigationbar
