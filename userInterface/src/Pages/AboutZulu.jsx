a2102d9288bf0cdff75c00a5cfd4ec3561575548
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-10-11 14:07:51 +0300

    Creation of the necessary classes in the pages section

---
+++ userInterface/src/Pages/AboutZulu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutZulu = () => {
+  return (
+    <div>
+      <h1>AboutZulu Component</h1>
+    </div>
+  );
+};
+
+export default AboutZulu;
---
+++ userInterface/src/Pages/Placeorder.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Placeorder = () => {
+  return (
+    <div>
+      <h1>Placeorder Component</h1>
+    </div>
+  );
+};
+
+export default Placeorder;
