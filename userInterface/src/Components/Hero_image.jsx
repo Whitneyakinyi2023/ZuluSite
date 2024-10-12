571750cb28bd3722c0481938993cd37c05360233
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-10-12 17:10:09 +0300

    Set up the appropriate Hero image

---
+++ userInterface/src/Components/Hero_image.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero_image = () => {
+    return (
+        <div>
+            +
+
+        </div>
+    )
+}
+
+export default Hero
--- userInterface/src/Components/Navigationbar.jsx
+++ userInterface/src/Components/Navigationbar.jsx
@@ -1,47 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import imagesData from '../assets/images.json';
 import './navbar.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Navigationbar = () => {
-    // Find the image with id 10
     const logoImage = imagesData.images.find(image => image.id === 10);
+    const searchImage = imagesData.images.find(image => image.id === 12);
+    const profileImage = imagesData.images.find(image => image.id === 13);
+    const cartImage = imagesData.images.find(image => image.id === 14);
+    const [dropdownOpen, setDropdownOpen] = useState(false); // State for dropdown visibility
+
+    // Toggle dropdown menu
+    const toggleDropdown = () => {
+        setDropdownOpen(prevState => !prevState);
+    };
 
     return (
         <div className='navbar'>
-            {/* Render the logo image */}
             {logoImage && (
-                <img src={new URL(`../assets/${logoImage.src}`, import.meta.url).href} alt={logoImage.name} className="navbar-logo" />
+                <img
+                    src={new URL(`../assets/${logoImage.src}`, import.meta.url).href}
+                    alt={logoImage.name}
+                    className="navbar-logo"
+                />
             )}
             <ul className='nav-list'>
-                <NavLink to='/' className='nav-item'>
-                    <p>Home</p>
-                </NavLink>
-                <NavLink to='/about' className='nav-item'>
-                    <p>About Zulu</p>
-                </NavLink>
-                <NavLink to='/drinks' className='nav-item'>
-                    <p>Products</p>
-                </NavLink>
-                <NavLink to='/order' className='nav-item'>
-                    <p>Order</p>
-                </NavLink>
-                <NavLink to='/history' className='nav-item'>
-                    <p>History</p>
-                </NavLink>
-                <NavLink to='/login' className='nav-item'>
-                    <p>Login</p>
-                </NavLink>
-                {/* <NavLink to='/contact' className='nav-item'>
-                    <p>Contact</p>
-                </NavLink> */}
-                <NavLink to='/cart' className='nav-item'>
-                    <p>Cart</p>
-                </NavLink>
-                <NavLink to='/profile' className='nav-item'>
-                    <p>Profile</p>
-                </NavLink>
+                <NavLink to='/' className='nav-item'><p>Home</p></NavLink>
+                <NavLink to='/about' className='nav-item'><p>About Zulu</p></NavLink>
+                <NavLink to='/drinks' className='nav-item'><p>Products</p></NavLink>
+                <NavLink to='/order' className='nav-item'><p>Order</p></NavLink>
             </ul>
+            <div className='profile' onClick={toggleDropdown}>
+                {/* Render the profile image */}
+                {profileImage && (
+                    <img
+                        src={new URL(`../assets/${profileImage.src}`, import.meta.url).href}
+                        alt={profileImage.name}
+                        className="profile-icon"
+                    />
+                )}
+                {dropdownOpen && (
+                    <div className='dropdown-menu'>
+                        <NavLink to='/profile' className='dropdown-item'>Profile</NavLink>
+                        <Link to='/cart' className='dropdown-item'>
+                            {/* Render the cart icon */}
+                            {cartImage && (
+                                <img
+                                    src={new URL(`../assets/${cartImage.src}`, import.meta.url).href}
+                                    alt={cartImage.name}
+                                    className="cart-icon"
+                                />
+                            )}
+                            Cart
+                        </Link>
+                        <NavLink to='/login' className='dropdown-item'>Login/Logout</NavLink>
+                        <NavLink to='/history' className='dropdown-item'>History</NavLink>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
