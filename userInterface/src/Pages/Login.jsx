6072962c521941f4016078d56e98ce6a37ab2f50
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-10-14 11:19:26 +0300

    State management for sign in and log in functionalities

--- userInterface/src/Pages/Login.jsx
+++ userInterface/src/Pages/Login.jsx
@@ -1,24 +1,37 @@
 import React, { useState } from 'react';
-import './Login.css'; // Import the CSS file for styling
+import './Login.css';
 
 const Login = () => {
   // useState hook to manage the current state of the form (Sign Up)
-  const [currentState, setCurrentState] = useState('Sign Up');
-
+  const [currentState, setCurrentState] = useState('LogIn');
+  const onSubmithandler = async (event) => {
+    event.PreventDefault();
+  }
   return (
     <div className='login-container'>
-      <form className='sign-up'>
+      <form onSubmit={onSubmithandler} className='sign-up'>
         <hr className='sign-style' />
         <div className='sign2'>
           <h2>{currentState}</h2> {/* Changed to h2 for better semantics */}
         </div>
-        <input type='text' className='name' placeholder='Name?' required />
-        <input type='email' className='email' placeholder='Valid Email?' required />
-        <input type='password' className='password' placeholder='Enter Password' required />
-        <button type='submit' className='submit-button'>Submit</button>
+        {currentState === 'LogIn' ? '' : <input type='text' className='name' placeholder='Name?' required />}
+        <input type='email' className='email' placeholder='Email?' required />
+        <input type='password' className='password' placeholder='Password' required />
+        <button className='submit-button'>{currentState === 'LogIn' ? 'Sign In' : 'Sign Up'} </button>
+
+        <div className='Forgot'>
+          <p className='reset-password'>Forgot your Password?</p>
+        </div>
+        {
+          currentState === 'LogIn'
+            ? <p onClick={() => setCurrentState('Sign Up')} className='account-creation'>Create an account</p>
+            : <p onClick={() => setCurrentState('LogIn')} className='log'>Log In </p>
+        }
       </form>
+
     </div>
   );
 };
+
 
 export default Login;
